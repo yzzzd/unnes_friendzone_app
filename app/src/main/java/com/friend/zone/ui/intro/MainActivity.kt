23fab1eca4b0829fbf33c65941044a94318099e0
Zidd <nuvyazid@gmail.com>
2022-01-16 11:06:24 +0700

    add session

--- app/src/main/java/com/friend/zone/ui/intro/MainActivity.kt
+++ app/src/main/java/com/friend/zone/ui/intro/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.friend.zone.R
+import com.friend.zone.data.Const
 import com.friend.zone.databinding.ActivityMainBinding
+import com.friend.zone.ui.home.HomeActivity
 import com.friend.zone.ui.login.LoginActivity
 import com.nuryazid.core.base.activity.BasicActivity
+import com.nuryazid.core.data.CoreSession
 
 class MainActivity : BasicActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +18,22 @@ class MainActivity : BasicActivity<ActivityMainBinding>(R.layout.activity_main) 
 
         Handler(Looper.getMainLooper()).postDelayed(
             {
-                val loginIntent = Intent(this, LoginActivity::class.java)
-                startActivity(loginIntent)
+                if (isLogin()) {
+                    val homeIntent = Intent(this, HomeActivity::class.java)
+                    startActivity(homeIntent)
+                } else {
+                    val loginIntent = Intent(this, LoginActivity::class.java)
+                    startActivity(loginIntent)
+                }
                 finish()
             }, 2000
         )
 
     }
+
+    private fun isLogin(): Boolean {
+        val user = CoreSession(this).getString(Const.SESSION.USER)
+        return user.isNotEmpty()
+    }
+    
 }
